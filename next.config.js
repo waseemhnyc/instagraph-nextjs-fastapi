f7fcd0f2b0282f2f2d40402efecd19192c6b160c
WaseemH <w927hussain@gmail.com>
2023-10-01 18:52:40 -0400

    changes to hit backend api

--- next.config.js
+++ next.config.js
@@ -6,11 +6,13 @@ const nextConfig = {
         source: '/api/:path*',
         destination:
           process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:5328/api/:path*'
+            ? 'http://127.0.0.1:5000/api/:path*'
+            : process.env.NODE_ENV === 'production'
+            ? 'https://instagraph-nextjs-production.up.railway.app/api/:path*'
             : '/api/',
       },
-    ]
+    ];
   },
-}
+};
 
 module.exports = nextConfig
